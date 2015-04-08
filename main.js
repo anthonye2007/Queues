6c13b96a02eacbc4396d603df512c539daf19f6c
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-04-08 12:07:29 -0400

    Restore web server

--- main.js
+++ main.js
@@ -90,11 +90,11 @@ app.get('/recent', function(req, res){
 })
 
 // HTTP SERVER
- //var server = app.listen(3000, function () {
+var server = app.listen(3000, function () {
 
-//   var host = server.address().address
-  // var port = server.address().port
+   var host = server.address().address
+   var port = server.address().port
 
-   //console.log('Example app listening at http://%s:%s', host, port)
-// })
+   console.log('Example app listening at http://%s:%s', host, port)
+})
 
